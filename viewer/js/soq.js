3a285b213299012ab562f3c9cbea9ba3b634cf9a
Andy Armstrong <andy@hexten.net>
2011-05-08 03:56:01 +0100

    Draw graph with two series.

--- viewer/js/soq.js
+++ viewer/js/soq.js
@@ -6,16 +6,12 @@ $(function() {
   if ('source' in url.args) {
     var source = url.makeAbsolute(url.args.source);
     console.log("source=" + source);
-    var ds = new DataSet(url);
+    var ds = new DataSet('root');
     ds.load(source, function() {
       console.log("data loaded");
-      var names = ds.names();
-      ds.eachSeries(function(path, series) {
-        var name = path.join('.');
-        var bounds = series.getBounds();
-        console.log(name + ": min=" + bounds.min + ", max=" + bounds.max);
-        $graph.graph('addSeries', series);
-      });
+      $graph.graph('addSeries', ds.get('media/cycling/q200.csv').get('ssim'));
+      $graph.graph('addSeries', ds.get('media/cycling/q1600.csv').get('ssim'));
+      $graph.graph('render');
     });
   }
 });
