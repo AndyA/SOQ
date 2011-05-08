cd128a365831dff0de95c4eb84b5c20263813d18
Andy Armstrong <andy@hexten.net>
2011-05-08 04:31:30 +0100

    Make it possible to specify the data type.

--- viewer/js/soq.js
+++ viewer/js/soq.js
@@ -2,15 +2,18 @@ $(function() {
   var $graph = $('#graph');
   $graph.graph();
 
-  var url = new URLParser(window.location.href);
+  var url = new URLParser(window.location.href, {
+    type: 'ssim'
+  });
   if ('source' in url.args) {
     var source = url.makeAbsolute(url.args.source);
     console.log("source=" + source);
     var ds = new DataSet('root');
+    var type = url.args.type;
     ds.load(source, function() {
       console.log("data loaded");
-      $graph.graph('addSeries', ds.get('media/cycling/q200.csv').get('ssim'));
-      $graph.graph('addSeries', ds.get('media/cycling/q1600.csv').get('ssim'));
+      $graph.graph('addSeries', ds.get('media/cycling/q200.csv').get(type));
+      $graph.graph('addSeries', ds.get('media/cycling/q1600.csv').get(type));
       $graph.graph('render');
     });
   }
