04006ac32120d9602211636d4c91a0ddf6af8085
Andy Armstrong <andy@hexten.net>
2011-05-08 03:55:43 +0100

    Plotting working.

--- viewer/js/graph.js
+++ viewer/js/graph.js
@@ -2,12 +2,130 @@
 
   var methods = {
     init: function(options) {
+      var data = {
+        settings: {},
+        ds: []
+      };
+
+      if (options) {
+        $.extend(data.settings, options);
+      }
+
+      this.data('graph', data);
       return this;
     },
     addSeries: function(ds) {
+      var data = this.data('graph');
+      data.ds.push(ds);
       return this;
     },
-    update: function() {
+    colourForPath: function(path) {
+      switch (path[path.length - 1]) {
+      case 'R':
+        return ([255, 0, 0]);
+      case 'G':
+        return ([0, 255, 0]);
+      case 'B':
+        return ([0, 0, 255]);
+      default:
+        return ([0, 0, 0]);
+      }
+    },
+    getBounds: function() {
+      var data = this.data('graph');
+      var w, h;
+      for (var i in data.ds) {
+        var sb = data.ds[i].getBounds();
+        if (i == 0) {
+          w = sb.len;
+          h = sb.max;
+        }
+        else {
+          w = Math.max(w, sb.len);
+          h = Math.max(h, sb.max);
+        }
+      }
+      return {
+        w: w,
+        h: h
+      };
+    },
+    getGraphArea: function() {
+      var cav = this.get(0);
+      return {
+        x: 0,
+        y: 0,
+        w: cav.width,
+        h: cav.height
+      };
+    },
+    niceCeiling: function(n) {
+      var seq = [2, 2.5, 2];
+      var sp = 0;
+      var nn = 0.001;
+      while (nn < n) {
+        nn *= seq[sp++];
+        if (sp == seq.length) sp = 0;
+      }
+      return nn;
+    },
+    getTransform: function() {
+      var ga = this.graph('getGraphArea');
+      var b = this.graph('getBounds');
+      var gh = this.graph('niceCeiling', b.h);
+      var xf = [ga.w / b.w, 0, 0, -ga.h / gh, ga.x, ga.h + ga.y];
+      return xf;
+    },
+    lineMaker: function(ctx) {
+      var xf = this.graph('getTransform');
+      var first = true;
+      return function(x, y) {
+        var xx = x * xf[0] + y * xf[2] + xf[4];
+        var yy = x * xf[1] + y * xf[3] + xf[5];
+        if (first) {
+          ctx.moveTo(xx, yy);
+          first = false;
+        }
+        else {
+          ctx.lineTo(xx, yy);
+        }
+      }
+    },
+    render: function() {
+      var data = this.data('graph');
+      var cav = this.get(0);
+      var ctx = cav.getContext('2d');
+      var $this = this;
+      var col = function(ar, alpha) {
+        return 'rgba(' + ar.concat([alpha]).join(', ') + ')';
+      }
+      for (var i in data.ds) {
+        data.ds[i].eachSeries(function(path, series) {
+          var pts = series.getPoints();
+          if (series.isComplex()) {
+            ctx.beginPath();
+            var lm = $this.graph('lineMaker', ctx);
+            for (var j in pts) {
+              lm(j, pts[j]['max']);
+            }
+            for (var j in pts) {
+              var jj = pts.length - 1 - j;
+              lm(jj, pts[jj]['min']);
+            }
+            ctx.closePath();
+            ctx.fillStyle = col($this.graph('colourForPath', path), 0.3);
+            ctx.strokeStyle = col($this.graph('colourForPath', path), 0.3);
+            ctx.fill();
+          }
+          ctx.beginPath();
+          var lm = $this.graph('lineMaker', ctx);
+          for (var j in pts) {
+            lm(j, pts[j]['avg']);
+          }
+          ctx.strokeStyle = col($this.graph('colourForPath', path), 1.0);
+          ctx.stroke();
+        });
+      }
       return this;
     }
   };
