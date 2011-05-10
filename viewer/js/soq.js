c2227ab3335df0cfb06c85d355f2019ffc9cd8ab
Andy Armstrong <andy@hexten.net>
2011-05-10 09:46:19 +0100

    Persist graph type in url frag.

--- viewer/js/soq.js
+++ viewer/js/soq.js
@@ -2,6 +2,8 @@ $(function() {
   var $graph = $('#graph');
   $graph.graph();
 
+  var url = new URLParser(window.location.href, {});
+
   function showSet(type) {
     console.log("displaying " + type);
     $graph.graph('init');
@@ -10,16 +12,15 @@ $(function() {
     $graph.graph('addSeries', ds.get('media/cycling/q1600.csv').get(type));
     $graph.graph('showSeries', 0, 0.3);
     $graph.graph('render');
+    url.part('frag', type);
+    window.location.href = url.toString();
   }
 
-  var url = new URLParser(window.location.href, {
-    type: 'ssim'
-  });
   if ('source' in url.args) {
     var source = url.makeAbsolute(url.args.source);
     console.log("source=" + source);
     var ds = new DataSet('root');
-    var type = url.args.type;
+    var type = url.part('frag') || 'psnr';
     ds.load(source, function() {
       console.log("data loaded");
       showSet(type);
